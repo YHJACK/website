6b767e36e4638358ba7baa8420b99527c05fc5c5
YHJACK <karlyh0629@gmail.com>
2023-03-07 10:49:09 +0900

    First Version

---
+++ Code/var.tf
@@ -0,0 +1,29 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "ap-northeast-1e"
+}
+
+variable "AWS_BLUEPRINT" {
+  default = "ubuntu_20_04"
+}
+
+variable "AWS_BUNDLE" {
+  default = "nano_2_0"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "./key/website_key"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "./key/website_key.pub"
+}
+
+variable "AWS_AVAILABILITY_ZONE" {
+  default = "ap-northeast-1a"
+}
